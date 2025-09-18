63f5d7f1b7d28f5738b760b6395cad50c6a87dc1
logith2006 <logith868@gmail.com>
2025-09-18 10:24:29 +0530

    divisible by both 3 and 5..
    java program using to divisible by both 3 and 5 ..

---
+++ divbythreeandfive.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class divbythreeandfive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println(number + " is divisible by both 3 and 5.");
+        } else {
+            System.out.println(number + " is not divisible by both 3 and 5.");
+        }
+
+        scanner.close();
+    }
+    
+}
